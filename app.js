4af5db72a68a150ae6882c4f155ab15fd970229b
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-02-08 08:49:00 -0800

    working with array and functions

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hello");
+// list = [2, 3, 4, 6];
+
+// for (let i = 0; i < list.length; i++) {
+//   console.log(list[i]);
+// }
+
+// list.pop();
+// list.push(7);
+
+// for (let item of list) {
+//   console.log(item);
+// }
+
+// function myFunc(num) {
+//   return num * 2;
+// }
+
+// console.log(myFunc(3));
