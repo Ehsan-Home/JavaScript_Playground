c0ad59bbeae896fa3d8f608835a2282a829233fd
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-02-08 09:32:43 -0800

    working with iteration through the objects

--- app.js
+++ app.js
@@ -28,11 +28,38 @@ obj = {
 // console.log(obj.age);
 // console.table(obj);
 
-delete obj.name;
+// delete obj.name;
 // obj.name = undefined;
 
-for (key in obj) {
-  console.log(key, obj[key]);
+// for (key in obj) {
+//   console.log(key, obj[key]);
+// }
+
+// let a = 4;
+// if (a == 4) {
+//   console.log("YES");
+// }
+
+list = [2, 3, 4, 6];
+
+// if (list.includes(8)) {
+//   console.log("3 is in the list");
+// }
+
+// let listKey = ["name", "id", "sex", "isDead", "height"];
+
+// for (key of listKey) {
+//   if (key in obj) {
+//     console.log(`${key} is in the obj`);
+//   }
+// }
+
+if (obj["name"] === "ali2") {
+  console.log("the value is good");
 }
+
+// if ("id" in obj) {
+//   console.log("id is in the obj");
+// }
 
 // console.table(obj);
