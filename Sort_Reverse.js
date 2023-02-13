239a4ca44d604fa5f68a827e142ed3923c5ae103
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-02-12 19:55:11 -0800

    working with sort and reverse

---
+++ Sort_Reverse.js
@@ -0,0 +1,15 @@
+// SORT
+
+let list = [3, 2, -1, 4, 40, 400, 5, 6];
+
+list.sort((a, b) => b - a);
+
+console.log(list);
+
+// REVERSE
+
+let list2 = [8, 7, 6, 5, -1, 10];
+
+list2.reverse();
+
+console.log(list2);
