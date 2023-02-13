142edd05fc49ef86100c29a30eeaf3fd02fdc15e
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-02-12 19:40:21 -0800

    working with filters

--- Filter.js
+++ Filter.js
@@ -2,4 +2,20 @@ let list = [1, 2, 3, 4, 5];
 
 let list2 = list.filter((item) => item < 3);
 
-console.log(list2);
+// console.log(list2);
+
+let listBoolean = [false, undefined, 0, 1, "ali", "", NaN];
+
+let str = "ehsan";
+
+if (str) {
+  console.log("The value is NOT null");
+} else {
+  console.log("The value is null");
+}
+
+console.log("ali" == true);
+
+let listBooleanFiltered = listBoolean.filter((item) => item);
+
+console.log(listBooleanFiltered);
